d5468507f33fcef6af1fa29284170c84fa136e87
Pinkieqt <izyman2@gmail.com>
2020-10-19 11:34:04 +0200

    Add files via upload
    Authentication and fixes

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de0c2db5481920ac1bea504731cda4ae.js"
+  "/precache-manifest.c87158d2e2f0d34d00384e9401980597.js"
 );
 
 self.addEventListener('message', (event) => {
