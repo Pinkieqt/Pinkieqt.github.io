ed75f67439ca2b2ed733ee2606b04a0ce4d1a769
Pinkieqt <izyman2@gmail.com>
2020-11-04 14:51:54 +0100

    Redesign

---
+++ precache-manifest.11e7198340745fb961efc22d8721d13d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ed68601f8420698fc0a56e6737a331",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e806040ee44ecc65ad5",
+    "url": "/static/css/2.e24804c5.chunk.css"
+  },
+  {
+    "revision": "f88fe9c1901e61500363",
+    "url": "/static/css/main.00af45aa.chunk.css"
+  },
+  {
+    "revision": "9e806040ee44ecc65ad5",
+    "url": "/static/js/2.563bf1d1.chunk.js"
+  },
+  {
+    "revision": "8b4df2b6b3cdd0bfbba7b4c7ba9e0d4d",
+    "url": "/static/js/2.563bf1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88fe9c1901e61500363",
+    "url": "/static/js/main.746414e2.chunk.js"
+  },
+  {
+    "revision": "68aea5d478313b084ddc",
+    "url": "/static/js/runtime-main.407e3717.js"
+  },
+  {
+    "revision": "a0511304041e981a39f480ee21e58907",
+    "url": "/static/media/bottomlogin.a0511304.svg"
+  },
+  {
+    "revision": "311cb54792bacdb638b21bfd60ab5bb0",
+    "url": "/static/media/icon.180fcaa7.png"
+  },
+  {
+    "revision": "3b2ba3770ccefdff052251576fdbbd25",
+    "url": "/static/media/toplogin.3b2ba377.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ef04425280a2510595b17a4d6d103c5.js"
+  "/precache-manifest.11e7198340745fb961efc22d8721d13d.js"
 );
 
 self.addEventListener('message', (event) => {
