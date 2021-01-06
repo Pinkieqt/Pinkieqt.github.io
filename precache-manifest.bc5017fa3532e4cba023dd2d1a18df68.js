457e7046ad081508296ca50216d55a443e202a19
Pinkieqt <izyman2@gmail.com>
2021-01-06 14:50:29 +0100

    Production build fix

---
+++ precache-manifest.bc5017fa3532e4cba023dd2d1a18df68.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8545595857025c0c048b1738b5c90ce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d589938444c5b411b975",
+    "url": "/static/css/2.e24804c5.chunk.css"
+  },
+  {
+    "revision": "53c948ce3b475b05248b",
+    "url": "/static/css/main.00af45aa.chunk.css"
+  },
+  {
+    "revision": "d589938444c5b411b975",
+    "url": "/static/js/2.c931b072.chunk.js"
+  },
+  {
+    "revision": "8b4df2b6b3cdd0bfbba7b4c7ba9e0d4d",
+    "url": "/static/js/2.c931b072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c948ce3b475b05248b",
+    "url": "/static/js/main.e54c2fc4.chunk.js"
+  },
+  {
+    "revision": "68aea5d478313b084ddc",
+    "url": "/static/js/runtime-main.407e3717.js"
+  },
+  {
+    "revision": "a0511304041e981a39f480ee21e58907",
+    "url": "/static/media/bottomlogin.a0511304.svg"
+  },
+  {
+    "revision": "311cb54792bacdb638b21bfd60ab5bb0",
+    "url": "/static/media/icon.180fcaa7.png"
+  },
+  {
+    "revision": "3b2ba3770ccefdff052251576fdbbd25",
+    "url": "/static/media/toplogin.3b2ba377.svg"
+  }
+]);
